07e5006423a7e63b0e2ee30d7d6009479a597089
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2021-06-21 15:43:31 -0500

    Fix image on Next

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from "next/head"
-import Image from "next/image"
-import Logo from "../public/assets/images/Logo.png"
 
 const Home = () => (
   <div>
     <Head>
       <title>Proyecto Savage</title>
     </Head>
-    <Image className="logo" src={Logo} alt="Proyecto Savage" />
+    <img className="logo" src="/assets/images/Logo.png" alt="Proyecto Savage" />
 
     <h3>Próximamente</h3>
   </div>
