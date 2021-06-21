a257f770fb79d2a55c4dfcd5c7fbcad2dff727ae
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2021-06-21 15:34:45 -0500

    Update cover

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,16 @@
-import Head from 'next/head';
+import Head from "next/head"
+import Image from "next/image"
+import Logo from "../public/assets/images/Logo.png"
 
 const Home = () => (
   <div>
     <Head>
-      <title>Next.js on the Jamstack with Netlify!</title>
+      <title>Proyecto Savage</title>
     </Head>
-    <h1>
-      Next.js on the <a href="https://jamstack.org">Jamstack</a>
-    </h1>
+    <Image className="logo" src={Logo} alt="Proyecto Savage" />
 
-    <h3>
-      Hooray 🎉 - you've built this with{' '}
-      <a href="https://nextjs.org">Next.js</a>!
-    </h3>
-
-    <style jsx>{`
-      :global(html, body) {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-      }
-
-      :global(body) {
-        font-size: calc(10px + 1vmin);
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-          'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-          'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        background-color: #282c34;
-        color: white;
-      }
-
-      a {
-        color: pink;
-        text-decoration: none;
-      }
-
-      .content {
-        padding: 0 32px;
-      }
-    `}</style>
+    <h3>Próximamente</h3>
   </div>
-);
+)
 
-export default Home;
+export default Home
