a803cae94865de8ddc5025a35a8438699712c3d5
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2021-11-19 03:23:52 -0600

    Send google Tag Manager experiment

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,32 @@
+import React from "react"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { GTM_ID, pageview } from "../lib/gtm"
 import "../styles/index.css"
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  React.useEffect(() => {
+    router.events.on("routeChangeComplete", pageview)
+    return () => {
+      router.events.off("routeChangeComplete", pageview)
+    }
+  }, [router.events])
+  return (
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ pages/test.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Test = () => {
+  return <div>Soy la página test</div>
+}
+
+export default Test
